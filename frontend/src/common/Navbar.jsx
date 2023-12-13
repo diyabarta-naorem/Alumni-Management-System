719fd0d25bf60fba693d8bf00a326a0b975c1d0a
Ricky <rickythoudam123@gmail.com>
2023-12-13 11:28:30 +0530

    adding new navbar and adjusting home page

--- frontend/src/common/Navbar.jsx
+++ frontend/src/common/Navbar.jsx
@@ -1,23 +1,42 @@
 import logo from '../assets/Logo.png';
+import mtuLogo from '../assets/Logo.png'
 export default function navbar(){
     return(
-      <div className=''>
-        <section id="navbar"> 
-          <div className="bg-whitee shadow-md flex flex-col items-center justify-around md:flex-row ">
-            <div>
-              <img src={logo} className='w-24 h-auto hidden md:block p-3'></img>
-            </div>
-            <ul className='flex flex-col md:flex-row text-black text-center w-full h-full md:w-auto'>
-              <li className='p-3 mx-1 text-lg hover:text-blue-700 hover:bg-gray-300 hover:rounded-2xl font-semibold '><a className='p-3 cursor-pointer block'>Home</a></li>
-              <li className='p-3 mx-1 text-lg hover:text-blue-700 hover:bg-gray-300 hover:rounded-2xl font-semibold '><a className='p-3 cursor-pointer block'>About</a></li>
-              <li className='p-3 mx-1 text-lg hover:text-blue-700 hover:bg-gray-300 hover:rounded-2xl font-semibold '><a className='p-3 cursor-pointer block'>Service</a></li>
-              <li className='p-3 mx-1 text-lg hover:text-blue-700 hover:bg-gray-300 hover:rounded-2xl font-semibold '><a className='p-3 cursor-pointer block'>Project</a></li>
-              <li className='p-3 mx-1 text-lg hover:text-blue-700 hover:bg-gray-300 hover:rounded-2xl font-semibold '><a className='p-3 cursor-pointer block'>Blog</a></li>
-              <li className='p-3 mx-1 text-lg hover:text-blue-700 hover:bg-gray-300 hover:rounded-2xl font-semibold '><a className='p-3 cursor-pointer block'>Pages</a></li>
-              <li className='p-3 mx-1 text-lg hover:text-blue-700 hover:bg-gray-300 hover:rounded-2xl font-semibold '><a className='p-3 cursor-pointer block'>Contact</a></li>
-           </ul>
-          </div>
-        </section>
-        </div>
+      
+
+<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+<a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <img src={mtuLogo} class="w-[3.7rem]" alt="Flowbite Logo"/>
+    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MTU Alumni</span>
+</a>
+<div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+    <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+      <span class="sr-only">Open main menu</span>
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+      </svg>
+  </button>
+</div>
+<div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+  <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    <li>
+      <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+    </li>
+    <li>
+      <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+    </li>
+    <li>
+      <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+    </li>
+    <li>
+      <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+    </li>
+  </ul>
+</div>
+</div>
+</nav>
+
     );
 }
--- frontend/src/common/home.jsx
+++ frontend/src/common/home.jsx
@@ -1,12 +1,13 @@
 import alumniHomeFoto from '../assets/alumniHomeFoto.jpg';
+import alumnniFotos from '../assets/alumniFotos.jpg'
 import '../index.css';
 export default function Home(){
     return(
         <div className=''>
           <div className=" relative group">
-            <img className="object-contain max-w-full max-h-full flex flex-row" src={alumniHomeFoto}/>
-              <div className="absolute top-0 left-0 w-3/4">
-                <h1 className='mx-10 mt-12 text-white text-3xl font-bold md:mt-20 md:mx-16 md:text-6xl'>Join, Connect, Thrive</h1>
+          <img className="object-contain max-w-full max-h-full flex flex-row" src={alumniHomeFoto}/>
+          <div className="absolute mt-24 top-0 left-0 w-3/4">
+                <h1 className='mx-10 mt-28 text-white text-3xl font-bold md:mt-20 md:mx-16 md:text-6xl'>Join, Connect, Thrive</h1>
                 <h2 className='mx-10 mt-4 text-white text-md font-medium md:mt-6 md:mx-16 md:text-2xl'>Students of Manipur Technical University</h2>
                 <p className='mx-10 my-4 text-white font-poppins md:mt-6 md:mx-16 '>We are delighted to welcome you to the our MTU Alumni community. 
                 This space is dedicated to fostering<br/> connections, celebrating achievements, and keeping you informed
@@ -18,6 +19,17 @@ export default function Home(){
                   <button className=' px-6 py-3 bg-white text-black font-medium md:w-3/12'>REGISTER</button>
                 </div>
               </div>
+          </div>
+          <div>
+            <div className='flex p-10 items-center justify-center flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0'>
+              <img className='w-1/2' src={alumnniFotos}/>
+              <div className=''>
+                <h1>Our mission</h1>
+                <p>Our mission is to foster lifelong connections, celebrate the achievements of our alumni, and provide a dynamic platform that promotes engagement, philanthropy, and professional growth. Through the MTU Alumni, we aim to build a community where every graduate feels valued,
+                 connected, and empowered to contribute to the ongoing success of our institution.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
     )
