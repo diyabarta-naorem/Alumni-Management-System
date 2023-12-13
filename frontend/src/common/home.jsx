df5dd3c54b936c85363354f09613aa84d52f930a
Ricky <rickythoudam123@gmail.com>
2023-12-13 17:49:34 +0530

    adding hamburger menu

--- frontend/src/common/home.jsx
+++ frontend/src/common/home.jsx
@@ -4,9 +4,9 @@ import '../index.css';
 export default function Home(){
     return(
         <div className=''>
-          <div className=" relative group">
-          <img className="object-contain max-w-full max-h-full flex flex-row" src={alumniHomeFoto}/>
-          <div className="absolute mt-24 top-0 left-0 w-3/4">
+          <div className=" relative group overflow-hidden h-screen min-h-[400px]">
+          <img className="h-full w-full object-cover" src={alumniHomeFoto}/>
+          <div className="absolute mt-24 top-0 left-0 w-full max-w-[600px]">
                 <h1 className='mx-10 mt-28 text-white text-3xl font-bold md:mt-20 md:mx-16 md:text-6xl'>Join, Connect, Thrive</h1>
                 <h2 className='mx-10 mt-4 text-white text-md font-medium md:mt-6 md:mx-16 md:text-2xl'>Students of Manipur Technical University</h2>
                 <p className='mx-10 my-4 text-white font-poppins md:mt-6 md:mx-16 '>We are delighted to welcome you to the our MTU Alumni community. 
@@ -14,18 +14,18 @@ export default function Home(){
                  about the exciting developments within<br/>our vibrant community.
                 </p>
                 
-                <div className='flex mx-16 mt-12 flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0'>
-                  <button className=' px-4 py-3 rounded-sm bg-Bluee text-white font-medium md:w-3/12'>COMMUNITY LOGIN</button>
-                  <button className=' px-4 py-3 rounded-sm bg-white text-black font-medium md:w-3/12'>REGISTER</button>
+                <div className='flex mx-16 mt-12 flex-col space-y-4 sm:flex-row sm:space-x-5 sm:space-y-0'>
+                  <button className=' px-4 py-2.5 rounded-md bg-Bluee text-white font-medium sm:max-w-max'>COMMUNITY LOGIN</button>
+                  <button className=' px-4 py-2.5 rounded-md bg-white text-black font-medium sm:max-w-max'>REGISTER</button>
                 </div>
               </div>
           </div>
           <div>
-            <div className='flex p-10 items-center justify-center flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0'>
-              <img className='w-1/2' src={alumnniFotos}/>
+            <div className='flex py-14 px-20 items-center justify-center flex-col space-y-4 md:flex-row md:space-x-10 md:space-y-0'>
+              <img className='w-1/3' src={alumnniFotos}/>
               <div className=''>
-                <h1>Our mission</h1>
-                <p>Our mission is to foster lifelong connections, celebrate the achievements of our alumni, and provide a dynamic platform that promotes engagement, philanthropy, and professional growth. Through the MTU Alumni, we aim to build a community where every graduate feels valued,
+                <h1 className='text-5xl font-bold'>Our mission</h1>
+                <p className='mt-4 font-normal text-gray-700'>Our mission is to foster lifelong connections, celebrate the achievements of our alumni, and provide a dynamic platform that promotes engagement, philanthropy, and professional growth. Through the MTU Alumni, we aim to build a community where every graduate feels valued,
                  connected, and empowered to contribute to the ongoing success of our institution.
                 </p>
               </div>
