e25d25e33ab90aac1099e09a82559eba54493d02
Naorem Diyabarta <naoremdiyabarta@gmail.com>
2023-12-14 12:29:38 +0530

    responsiveness

--- frontend/src/common/Navbar.jsx
+++ frontend/src/common/Navbar.jsx
@@ -66,16 +66,12 @@ export default function Navbar() {
                     </a>
                   </li>
                   <li>
-<<<<<<< HEAD
-                    <a to=""
-=======
-                    <button onClick={ContactPage}
-                      href="#"
->>>>>>> 3b2b5e87a9e5bbc437f02b5826ad07cff9cfc789
+                    <a
+
                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                     >
                       Contact
-                    </button>
+                    </a>
                   </li>
                 </ul>
               </div>
--- frontend/src/common/contact.jsx
+++ frontend/src/common/contact.jsx
@@ -1,13 +1,13 @@
 export default function Contact(){
     return(
         <div>
-<<<<<<< HEAD
+
           <div>
             <h1>This is contact page</h1>
           </div>
-=======
+
             <h1>This is a contact page</h1>
->>>>>>> 3b2b5e87a9e5bbc437f02b5826ad07cff9cfc789
+
         </div>
     );
 }
