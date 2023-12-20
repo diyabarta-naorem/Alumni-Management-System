8ebda51334d313043ec844d1302a78194e04798a
Ricky <rickythoudam123@gmail.com>
2023-12-20 18:31:43 +0530

    nothing

--- frontend/src/common/Navbar.jsx
+++ frontend/src/common/Navbar.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-import mtuLogo from '../assets/Logo.png';
+import React, { useState } from "react";
+import mtuLogo from "../assets/Logo.png";
 import { NavLink } from "react-router-dom";
-
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,20 +12,24 @@ export default function Navbar() {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600 z-40">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-        <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <NavLink
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
           <img src={mtuLogo} className="h-8 md:h-12" alt="MTU Logo" />
-          <span className="self-center text-base md:text-lg font-semibold whitespace-nowrap dark:text-white">MTU Alumni</span>
+          <span className="self-center text-base md:text-lg font-semibold whitespace-nowrap dark:text-white">
+            MTU Alumni
+          </span>
         </NavLink>
         <div className="flex flex-wrap md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-         <a href="/login">
-        
-          <button
-            type="button"
-            className="text-white px-3 py-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-4 md:py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Get started
-          </button>
-         </a>
+          <a href="/login">
+            <button
+              type="button"
+              className="text-white px-3 py-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm md:px-4 md:py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Get started
+            </button>
+          </a>
           <button
             onClick={toggleMenu}
             type="button"
@@ -34,14 +37,28 @@ export default function Navbar() {
             aria-expanded={isMenuOpen}
           >
             <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
             </svg>
           </button>
         </div>
       </div>
       <div
-        className={`items-center justify-between w-full ${isMenuOpen ? 'block' : 'hidden'} md:flex md:w-auto md:order-1`}
+        className={`items-center justify-between w-full ${
+          isMenuOpen ? "block" : "hidden"
+        } md:flex md:w-auto md:order-1`}
         id="navbar-sticky"
       >
         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -56,7 +73,7 @@ export default function Navbar() {
           </li>
           <li>
             <NavLink
-              to='/aboutUs'
+              to="/aboutUs"
               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
             >
               About
@@ -64,7 +81,7 @@ export default function Navbar() {
           </li>
           <li>
             <a
-              href='/services'
+              href="/services"
               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
             >
               Services
@@ -72,7 +89,7 @@ export default function Navbar() {
           </li>
           <li>
             <a
-              href='/contact'
+              href="/contact"
               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
             >
               Contact
