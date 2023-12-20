cb2b315a2060fdd780a3bfe047d5dbb9b6bcfd6f
Naorem Diyabarta <naoremdiyabarta@gmail.com>
2023-12-20 17:34:09 +0530

    no reload in the page - inProgress

--- frontend/src/common/Navbar.jsx
+++ frontend/src/common/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import mtuLogo from '../assets/Logo.png';
+import { NavLink } from "react-router-dom";
 
 
 export default function Navbar() {
@@ -12,10 +13,10 @@ export default function Navbar() {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600 z-40">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={mtuLogo} className="h-8 md:h-12" alt="MTU Logo" />
           <span className="self-center text-base md:text-lg font-semibold whitespace-nowrap dark:text-white">MTU Alumni</span>
-        </a>
+        </NavLink>
         <div className="flex flex-wrap md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
          <a href="/login">
         
@@ -54,12 +55,12 @@ export default function Navbar() {
             </a>
           </li>
           <li>
-            <a
-              href='/aboutUs'
+            <NavLink
+              to='/aboutUs'
               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
             >
               About
-            </a>
+            </NavLink>
           </li>
           <li>
             <a
